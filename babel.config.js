e851d758d7ccf45f296622c95b90d0c80d6ebb1c
eduardo santos <eduardo.santos99@outllok.com>
2024-07-24 20:31:12 -0300

    init project

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,31 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
+
+  const plugins = [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        root: ["./src/"],
+        alias: {
+          "@assets": "./src/assets",
+          "@components": "./src/components",
+          "@config": "./src/config",
+          "@routes": "./src/routes",
+          "@views": "./src/views",
+        }
+      }
+    ],
+    ['module:react-native-dotenv', 
+      {
+        "envName": "APP_ENV",
+        "moduleName": "@env",
+        "path": ".env",
+      }
+    ]
+  ];
+
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: plugins,
   };
 };
